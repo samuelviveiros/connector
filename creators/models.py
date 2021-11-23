3ddd25cc403e4de51baa6c82dcb1e795b78e73ad
Dartz <samuel.viveiros@gmail.com>
2021-11-23 00:04:31 -0300

    Add model `Creator`
    Related to #3

--- creators/models.py
+++ creators/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Creator(models.Model):
+    name = models.CharField(max_length=255)
+    phone = models.CharField(max_length=255, verbose_name='Phone number')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Created at')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Updated at')
+    created_by = models.ForeignKey(User, verbose_name='Created by', related_name='creators', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name

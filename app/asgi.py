92dbec5512373860327c7d2cfa30e73733bbdc29
Dartz <samuel.viveiros@gmail.com>
2021-11-29 10:14:19 -0300

    Experimenting with Daphne
    Related to #13

--- app/asgi.py
+++ app/asgi.py
@@ -7,21 +7,41 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 """
 
+# import os
+
+# from channels.auth import AuthMiddlewareStack
+# from channels.routing import ProtocolTypeRouter, URLRouter
+# from django.core.asgi import get_asgi_application
+
+# from chat.websocket.routing import websocket_urlpatterns
+
+
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
+
+# application = ProtocolTypeRouter({
+#     'http': get_asgi_application(),
+#     'websocket': AuthMiddlewareStack(
+#         URLRouter(websocket_urlpatterns)
+#     ),
+#     # We can add other protocols here
+# })
+
+
 import os
 
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
-from django.core.asgi import get_asgi_application
+import django
 
 from chat.websocket.routing import websocket_urlpatterns
 
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
 
+django.setup()
+
 application = ProtocolTypeRouter({
-    'http': get_asgi_application(),
     'websocket': AuthMiddlewareStack(
         URLRouter(websocket_urlpatterns)
     ),
-    # We can add other protocols here
 })
